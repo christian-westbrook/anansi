5cdf24b6d8c32086060ef1ba0b0baaeb03492e5a
Christian Westbrook <christianwestbrook@live.com>
2021-05-12 21:12:37 -0500

    MVP-2 Added index.php and index.css

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+			<title>christianwestbrook.dev</title>
+			<link type="text/css" rel="stylesheet" href="./styles/index.css" />
+	</head>
+
+	<body>
+		<div id="header">
+			<h3>christianwestbrook.dev</h3>
+		</div>
+	</body>
+</html>
