81b60ef2f5b412a8f5e05e2ae1df3b38951b6cb6
Christian Westbrook <christianwestbrook@live.com>
2021-05-15 03:19:46 -0500

    MVP-3 Added loop for blog posts to index.php

--- public/index.php
+++ public/index.php
@@ -3,11 +3,29 @@
 	<head>
 			<title>christianwestbrook.dev</title>
 			<link type="text/css" rel="stylesheet" href="./styles/index.css" />
+			<link type="text/css" rel="stylesheet" href="./styles/blog.css" />
+
+			<?php
+				include './modules/blog-utilities.php';
+			?>
 	</head>
 
 	<body>
 		<div id="header">
 			<h3>christianwestbrook.dev</h3>
 		</div>
+
+		<?php
+			foreach(scandir('./data/') as $path) {
+
+				if ($path == '.' || $path == '..')
+					continue;
+
+				$blog = extractBlogFromXML('./data/' . $path);
+				$blog = transformBlog($blog);
+				
+				echo $blog;
+			}
+		?>
 	</body>
 </html>
