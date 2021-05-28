a166afd1096064b1a877fd6a85238a2eb23c19a0
Christian Westbrook <christianwestbrook@live.com>
2021-05-28 15:45:52 -0500

    MVP-7 Updated BlogEngine class with new generateBlogFeed() method

--- public/classes/BlogEngine.php
+++ public/classes/BlogEngine.php
@@ -1,4 +1,7 @@
 <?php
+
+include './modules/blog-utilities.php';
+
 # ------------------------------------------------------------------------------
 # Class    : BlogEngine.php
 # Engineer : Christian Westbrook
@@ -19,6 +22,35 @@ class BlogEngine {
 	# --------------------------------------------------------------------------
 	function __construct($blogSearchPath) {
 		$this->blogSearchPath = $blogSearchPath;
+	}
+
+	# --------------------------------------------------------------------------
+	# Function   : generateBlogFeed()
+	# Engineer   : Christian Westbrook
+	# Abstract   : This function generates a stream of HTML content representing
+	#              a blog post feed from all blog posts found in the blog search
+	#              path.
+	# --------------------------------------------------------------------------
+	public function generateBlogFeed() {
+		// Array to store extracted blog posts
+		$blogs = array();
+
+		// For every blog file in ./data
+		foreach(scandir($this->blogSearchPath) as $path) {
+			if ($path == '.' || $path == '..' || $path == 'demo.xml')
+				continue;
+
+			// Extract blog content from the file at the given path
+			// and append the content to the $blogs array
+			array_push($blogs, extractBlogFromXML('./data/' . $path));
+		}
+
+		// Sort blogs on the date field
+		array_multisort(array_column($blogs, "date"), SORT_DESC, $blogs);
+
+		foreach($blogs as $blog) {
+			echo transformBlog($blog);
+		}
 	}
 }
 ?>
