6d48a2124b2d87a95db4340aad2400805d96a318
Christian Westbrook <christianwestbrook@live.com>
2021-05-28 15:46:14 -0500

    MVP-7 Updated index.php to use new BlogEngine class

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 			<title>christianwestbrook.dev</title>
 			<link type="text/css" rel="stylesheet" href="./styles/index.css" />
 			<link type="text/css" rel="stylesheet" href="./styles/blog.css" />
-			<?php include './modules/blog-utilities.php'; ?>
+			<?php include './classes/BlogEngine.php'; ?>
 	</head>
 
 	<body>
@@ -13,26 +13,9 @@
 		</div>
 
 		<?php
-			// Array to store extracted blog posts
-			$blogs = array();
-
-			// For every blog file in ./data
-			foreach(scandir('./data/') as $path) {
-				if ($path == '.' || $path == '..' || $path == 'demo.xml')
-					continue;
-
-				// Extract blog content from the file at the given path
-				// and append the content to the $blogs array
-				array_push($blogs, extractBlogFromXML('./data/' . $path));
-			}
-
-			// Sort blogs on the date field
-			array_multisort(array_column($blogs, "date"), SORT_DESC, $blogs);
-
-			foreach($blogs as $blog) {
-				echo transformBlog($blog);
-			}
-
+			$blogEngine = new BlogEngine('./data/');
+			$blogFeed = $blogEngine->generateBlogFeed();
+			echo $blogFeed;
 		?>
 	</body>
 </html>
